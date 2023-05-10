8219b5ff52f90607a32f6ac826a113c2bb77ceef
jia-son <116625758+jia-son@users.noreply.github.com>
2023-05-10 15:05:33 +0900

    파트 2.11 진행

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const age = 96;
 function calculateKrAge(ageOfForeigner) {
-    return ageOfForeigner + 2;
+    return ageOfForeigner + 2; //리턴값이 주어져야 값이 적용됨
 }
 
 const krAge = calculateKrAge(age);
