21b7c88a4f6a8702ee2ad3dca0855e977bbe0b90
jia-son <116625758+jia-son@users.noreply.github.com>
2023-05-15 16:15:38 +0900

    파트 4.4 진행

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 const loginForm = document.getElementById("login-form");
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
-
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSumbit(event) {
     event.preventDefault();
-    console.log(loginInput.value);
-}
-
-
-function handleLinkClick(event) {
-    event.preventDefault();
-    console.dir(event);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    greeting.innerText =`Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSumbit);
-link.addEventListener("click", handleLinkClick)
