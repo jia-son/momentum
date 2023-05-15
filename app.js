414be88f8630280cd09dad0b658f583c4d523b72
jia-son <116625758+jia-son@users.noreply.github.com>
2023-05-15 15:39:48 +0900

    파트 4.3 진행

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const loginForm = document.getElementById("login-form");
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
 
+const link = document.querySelector("a");
+
 
 function onLoginSumbit(event) {
     event.preventDefault();
-    // const username = loginInput.value;
     console.log(loginInput.value);
 }
 
+
+function handleLinkClick(event) {
+    event.preventDefault();
+    console.dir(event);
+}
+
 loginForm.addEventListener("submit", onLoginSumbit);
+link.addEventListener("click", handleLinkClick)
