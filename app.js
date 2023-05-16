e5f62fc3dabf144624db03ef5c5c1a78973fd030
jia-son <116625758+jia-son@users.noreply.github.com>
2023-05-16 15:34:28 +0900

    파트 4.5 진행

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ function onLoginSumbit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
+    localStorage.setItem("username", username);
     greeting.innerText =`Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
