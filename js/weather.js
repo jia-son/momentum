ab8e6457a96c193fe437709b971e66efcb71c1e6
jia-son <116625758+jia-son@users.noreply.github.com>
2023-05-18 17:18:32 +0900

    파트 8.1 진행

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,15 @@
+const API_KEY = "27875c199b8fa9292d60d13d92ce2b06"; // 자신의 API KEY 입력
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log(`You live it ${lat} ${lng}`);
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json()).then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 
 function onGeoError() {
